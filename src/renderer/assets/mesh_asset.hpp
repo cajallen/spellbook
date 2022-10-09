93fb96148fbea1cd3a51108282d9d31e1201a586
cajallen <cajallen427@gmail.com>
2022-10-09 16:11:15 -0700

    Header fix

--- src/renderer/assets/mesh_asset.hpp
+++ src/renderer/assets/mesh_asset.hpp
@@ -21,16 +21,15 @@ namespace spellbook {
 
 struct MeshInfo {
     CompressionMode compression_mode = {};
-    u64             vertices_bsize   = 0;
-    u64             indices_bsize    = 0;
+    u32             vertices_bsize   = 0;
+    u32             indices_bsize    = 0;
     u32             index_bsize      = 0;
 
     MeshInfo() = default;
     JSON_IMPL(MeshInfo, compression_mode, vertices_bsize, indices_bsize, index_bsize);
 };
 
-MeshInfo   read_mesh_info(AssetFile* file);
-void       unpack_mesh(MeshInfo* info, const u8* source_buffer, u64 source_size, u8* vertices, u8* indices);
-void       save_mesh(MeshCPU& mesh_cpu);
+MeshCPU load_mesh(const string& file_name);
+void    save_mesh(const MeshCPU& mesh_cpu);
 
 }
